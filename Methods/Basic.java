17e3c74b060e127f91c53403488ba4243b62623b
Sahil Agiwal <agiwasx21@juniata.edu>
2022-10-26 07:42:54 -0400

    added Methods

---
+++ Methods/Basic.java
@@ -0,0 +1,17 @@
+package Methods;
+
+public class Basic {
+    public static void main(String args[])
+    {
+        // Basic basic=new Basic();
+        System.out.println(basic.max(2,8));
+    }
+    static int max (int x,int y){
+        if(x>y){
+            return x;
+        }
+        else{
+            return y;
+        }
+    }
+}
---
+++ Methods/PassingObject.java
@@ -0,0 +1,8 @@
+package Methods;
+
+public class PassingObject {
+    public static void main(String args[]){
+        
+    }
+    
+}
